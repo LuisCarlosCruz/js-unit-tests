5ced062fe58bbdd97024a5bb3cdb0d4598d4adef
luis carlos <luizcruz0809@gmail.com>
2021-06-23 11:15:07 -0300

    lint

--- src/vqv.js
+++ src/vqv.js
@@ -21,6 +21,9 @@ const vqv = (nome, idade) => {
     return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`; } return undefined; };
+#VQV!`; 
+} 
+return undefined; 
+};
 vqv();
 module.exports = vqv;
